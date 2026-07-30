54bb9eb1d55c211a21623a1753201d614dc7e040
Modestas Pocius <modestas@bigmatrix.co.uk>
2026-07-30 10:12:10 +0100

    Changed fastMCP

--- src/mailchimp_mcp_server/server.py
+++ src/mailchimp_mcp_server/server.py
@@ -4,7 +4,7 @@ import os
 from typing import Optional
 
 import requests
-from mcp.server.fastmcp import FastMCP
+from fastmcp import FastMCP
 
 # --- Config ---
 MAILCHIMP_API_KEY = os.environ.get("MAILCHIMP_API_KEY", "")
